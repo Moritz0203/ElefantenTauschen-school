cd2f6b7390db3a8f02c3f529a9f6ef3eb32bb008
Moritz0203 <104513008+Moritz0203@users.noreply.github.com>
2023-09-28 17:38:13 +0200

    Add project files.

---
+++ ElefantenTauschen/Elefant.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ElefantenTauschen
+{
+    class Elefant
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                MessageBox.Show("meinen Namen kannst du nicht ändern.");
+
+            }
+        }
+        int ohrengroesse;
+        public int Ohrengröße
+        {
+            get { return ohrengroesse; }
+            set { }
+        }
+        int elefantenGröße;
+        public int ElefantenGröße
+        {
+            get { return elefantenGröße; }
+            set
+            {
+                elefantenGröße = value;
+                ohrengroesse = elefantenGröße / 5;
+            }
+        }
+
+        public Elefant(string Elefantenname, int Größe)
+        {
+            name = Elefantenname;
+            ElefantenGröße = Größe;
+            MessageBox.Show("Hier bin ich: " + name + ". Ich bin " + elefantenGröße + "cm groß und meine Ohren sind " + ohrengroesse + "cm lang.");
+        }
+
+        public string WerBinIch()
+        {
+            return "Hallo! Ich bin " + name + ". Meine Ohren sind " + ohrengroesse + "cm lang.";
+        }
+
+        public string WieGroßBinIch()
+        {
+            return "Hallo! Ich bin " + name + ". Ich bin " + elefantenGröße + " groß.";
+        }
+    }
+}
---
+++ ElefantenTauschen/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElefantenTauschen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Elefant ersterElefant, zweiterElefant, ZwischenspeicherElefant;
+        bool oneTime = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_ersterElefant_Click(object sender, RoutedEventArgs e)
+        {
+            if (ersterElefant == null)
+            {
+                ersterElefant = new Elefant("Ermir", 80);
+                bt_ersterElefant.Content = ersterElefant.Name;
+                bt_Wachsen.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show(ersterElefant.WerBinIch());
+            }
+
+            if (ersterElefant != null && zweiterElefant != null && oneTime == false)
+            {
+                bt_Tauschen.IsEnabled = true;
+                oneTime = true;
+            }
+        }
+
+        private void bt_zweiterElefant_Click(object sender, RoutedEventArgs e)
+        {
+            if (zweiterElefant == null)
+            {
+                zweiterElefant = new Elefant("Jonas", 60);
+                bt_zweiterElefant.Content = zweiterElefant.Name;
+            }
+            else
+            {
+                MessageBox.Show(zweiterElefant.WerBinIch());
+            }
+
+            if (ersterElefant != null && zweiterElefant != null && oneTime == false)
+            {
+                bt_Tauschen.IsEnabled = true;
+                oneTime = true;
+            }
+        }
+
+        private void bt_Wachsen_Click(object sender, RoutedEventArgs e)
+        {
+            ersterElefant.ElefantenGröße += 5;
+            TextBlockStatus.Text = ersterElefant.WieGroßBinIch();
+        }
+
+        private void bt_Tauschen_Click(object sender, RoutedEventArgs e)
+        {
+            ZwischenspeicherElefant = ersterElefant;
+            ersterElefant = zweiterElefant;
+            zweiterElefant = ZwischenspeicherElefant;
+            TextBlockStatus.Text = "";
+            bt_Name();
+        }
+
+        private void bt_Name()
+        {
+            bt_ersterElefant.Content = ersterElefant.Name;
+            bt_zweiterElefant.Content = zweiterElefant.Name;
+        }
+
+    }
+}
